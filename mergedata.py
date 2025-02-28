51e3ecef04e976af49c34005a130666595f09827
minhvu05 <minhvu0514@gmail.com>
2025-02-28 04:52:50 -0500

    complete

--- mergedata.py
+++ mergedata.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import glob
 
-def list_common_items(file1, file2, file3, ans_file):
+def merge_data(file1, file2, file3, ans_file):
     # Creating dataframes from given text files
     df1 = pd.read_csv('file1.txt', header=None, names=['word'])
     df2 = pd.read_csv('file2.txt', header=None, names=['word'])
@@ -13,18 +13,30 @@ def list_common_items(file1, file2, file3, ans_file):
     df3['file3'] = 'yes'
 
     # Merge all dataframes into 1
-    df1 = pd.merge(df1, df2[['word']], on='word', how='left').fillna({'file2': 'no'})
-    df1 = pd.merge(df1, df2[['word']], on='word', how='left').fillna({'file3': 'no'})
+    merged_df = pd.merge(df1[['word', 'file1']], df2[['word', 'file2']], on='word', how='outer')
+    merged_df = pd.merge(merged_df, df3[['word', 'file3']], on='word', how='outer')
 
-    final_df = pd.concat([df1[['word', 'file1', 'file2', 'file3']], 
-                        df2[['word', 'file2']], 
-                        df3[['word', 'file3']]], 
-                        axis=0).drop_duplicates(subset=['word']).reset_index(drop=True)
+    # Replace all the NaN values with no
+    merged_df['file1'] = merged_df['file1'].fillna('no')
+    merged_df['file2'] = merged_df['file2'].fillna('no')
+    merged_df['file3'] = merged_df['file3'].fillna('no')
 
+    # Create a count to see when and which column to put yes in
+    merged_df['count'] = merged_df[['file1', 'file2', 'file3']].apply(lambda row: row == 'yes', axis=1).sum(axis=1)
 
+    # Formatting the dataframe
+    merged_df = merged_df[['word', 'file1', 'file2', 'file3']]
+    merged_df.columns = ['data', file1, file2, file3]
 
-    
+    # Brute Force remove the data row
+    merged_df = merged_df[~((merged_df['data'] == 'data') & 
+                                      (merged_df[file1] == 'yes') & 
+                                      (merged_df[file2] == 'yes') & 
+                                      (merged_df[file3] == 'yes'))]
 
+    merged_df.to_csv(ans_file, index=False, sep=';', header=True)
 
+  
 
-list_common_items("file1.txt", "file2.txt", "file3.txt", "ans.txt")
+merge_data("file1.txt", "file2.txt", "file3.txt", "ans.txt")
+
